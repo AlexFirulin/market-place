3bb35bad75a7aeb393c99c346438e1594a3b08a1
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2024-01-30 19:59:41 +0200

    Add cross origin

--- backend/src/main/java/com/kazmiruk/marketplace/controller/TestController.java
+++ backend/src/main/java/com/kazmiruk/marketplace/controller/TestController.java
@@ -2,11 +2,13 @@ package com.kazmiruk.marketplace.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/test")
+@CrossOrigin("http://localhost:3000/")
 public class TestController {
 
     @GetMapping("/hello")
