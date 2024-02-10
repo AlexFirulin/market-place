af75823b2ebbdf8c9480a61f81b02ac5a3d0b6c6
AlexFirulin <1993firulin@gmail.com>
2024-02-10 12:06:32 +0200

    udpdate header

---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="main-page">
+    <legend class="main-legend">
+      <img src="~/assets/images/man-page.jpeg" alt="black-box" class="legend-image">
+      <div class="legend-info">
+        <h2>Чорна скринька</h2>
+      </div>
+    </legend>
+  </main>
+</template>
+
+<style lang="scss">
+@use "~/assets/scss/pages/the-main-page.scss"
+</style>
