455001ccb245f3dc31163b4fd1d6085ebbfe439d
AlexFirulin <1993firulin@gmail.com>
2024-02-16 23:42:09 +0200

    update footer

---
+++ frontend/pages/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="login-wrapper">
+  <div class="mt-2 h-2"></div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">@use "~/assets/scss/pages/login-page.scss"</style>
