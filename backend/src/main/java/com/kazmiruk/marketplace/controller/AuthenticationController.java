4dbbe2cd4d658d609771b9ad0fca6403f5013329
Dmytro Kazmiruk <dima.kazmiruk.05@gmail.com>
2024-03-07 10:29:58 +0200

    Add request fields validation

--- backend/src/main/java/com/kazmiruk/marketplace/controller/AuthenticationController.java
+++ backend/src/main/java/com/kazmiruk/marketplace/controller/AuthenticationController.java
@@ -4,13 +4,20 @@ import com.kazmiruk.marketplace.dto.AuthenticationRequest;
 import com.kazmiruk.marketplace.dto.AuthenticationResponse;
 import com.kazmiruk.marketplace.dto.RegisterRequest;
 import com.kazmiruk.marketplace.service.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
 
 @Controller
 @RequestMapping("/api/auth")
@@ -22,7 +29,7 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<?> register(
-            @RequestBody RegisterRequest request
+            @Valid @RequestBody RegisterRequest request
     ) {
         try {
             return ResponseEntity.ok(service.register(request));
@@ -36,5 +43,19 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exp
+    ) {
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError) error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
--- backend/src/main/java/com/kazmiruk/marketplace/entity/User.java
+++ backend/src/main/java/com/kazmiruk/marketplace/entity/User.java
@@ -24,7 +24,6 @@ public class User implements UserDetails {
     @GeneratedValue
     private Long id;
     private String firstName;
-    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
